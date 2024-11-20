54b6c2e5f98c950347b440722ae317737d7baf0d
Chris0546 <302717645@student.rocmondriaan.nl>
2024-11-20 14:37:12 +0100

    versie 1

---
+++ Opdracht 14/main.js
@@ -0,0 +1,28 @@
+function printHelloWorld(){
+   console.log("Hello World");
+}
+
+printHelloWorld();
+
+//
+
+function doubleNumber(){
+    console.log(5 ==+ 6);
+}
+doubleNumber();
+
+//
+
+function sumTwoNumbers(){
+    console.log(5 + 6);
+}
+
+sumTwoNumbers();
+
+
+function toUpperCase(){
+    const firstName = 'Chris'
+    console.log(firstName)
+}
+
+toUpperCase();
