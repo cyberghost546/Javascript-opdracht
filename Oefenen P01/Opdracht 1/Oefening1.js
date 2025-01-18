377a967c28b06ac2946f672fedd71911a832f571
christopher <christophermolina@outlook.com>
2025-01-18 22:44:27 +0100

    main

---
+++ Oefenen P01/Opdracht 1/Oefening1.js
@@ -0,0 +1,21 @@
+let className = 'B1J';
+let isHomeworkSubmittedOnTime = 'yes';
+let nextExamDate = '22 jan 2025'
+let numberOfPresentStudents = 13;
+let homeworkSubmissionRate = 3;
+let teachersList = 'list all'; 
+let averageTeacherAge = 24;
+let isCanteenOpenToday = 'yes';
+let schoolOpeningTime = '8:30';
+let numberOfComputers = 53;
+
+console.log(className);
+console.log(isHomeworkSubmittedOnTime);
+console.log(nextExamDate);
+console.log(numberOfPresentStudents);
+console.log(homeworkSubmissionRate);
+console.log(teachersList);
+console.log(averageTeacherAge);
+console.log(isCanteenOpenToday);
+console.log(schoolOpeningTime );
+console.log(numberOfComputers);
---
+++ Oefenen P01/Opdracht 2/main.js
@@ -0,0 +1,12 @@
+const schoolJaar = '2024';
+console.log(schoolJaar);
+
+const fietsenRekken = 32;
+console.log(fietsenRekken);
+
+const studieRichtingen = 'ict';
+console.log(studieRichtingen);
+
+let gemiddeldeCijfer = 6.8;
+console.log(gemiddeldeCijfer);
+
